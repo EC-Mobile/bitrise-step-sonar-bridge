613d01743203b4ae835050aa47c3672e6ef5615f
Chiong, Gianne | Gian | ECMD <gianne.chiong@rakuten.com>
2021-01-28 10:24:38 +0900

    Make parameters optional (except for output-directory)
    Some of the parameters are now optional in case the devs want to use the config in their .slather.yml file. The output-directory param will have to be fixed since the script for reading the coverage file will have to be known in Jenkins -- where there's another script that actually performs the Sonar operation).

--- step.sh
+++ step.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-slather coverage --llvm-cov --output-directory "llvm-cov" --binary-basename "$binary_basename" --workspace "$workspace" --scheme "$scheme" "$project"
+slather coverage --llvm-cov --output-directory "llvm-cov" ${binary_basename:+--binary-basename "$binary-basename"} ${workspace:+--workspace "$workspace"} ${scheme:+--scheme "$scheme"} ${project:+"$project"}
 mv llvm-cov/report.llcov $BITRISE_DEPLOY_DIR
 
 curl -X "POST" "https://$gitpub_sonar_bridge_host/rest/api/1.0/projects/$gitpub_sonar_bridge_owner/repos/$gitpub_sonar_bridge_repo/commits/$gitpub_sonar_bridge_commit_id/comments?path=$gitpub_sonar_bridge_file_path" \
