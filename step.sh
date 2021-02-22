cc9519b0bdacf38c43e74cb7a1458ffd57d69591
Isaac, Danilo | Dean | ECMD <danilo.isaac@rakuten.com>
2021-02-22 10:58:39 +0900

    Added Android support - added new input parameter: `platform` and `android_jacoco_xml` - renamed input parameters: `ios_workspace`, `ios_project`, `ios_scheme` - added conditional logic for iOS and Android platform test report export

--- step.sh
+++ step.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 set -ex
 
-slather coverage --llvm-cov --output-directory "llvm-cov" ${binary_basename:+--binary-basename "$binary_basename"} ${workspace:+--workspace "$workspace"} ${scheme:+--scheme "$scheme"} ${project:+"$project"}
-mv llvm-cov/report.llcov $BITRISE_DEPLOY_DIR
+# check platform
+lowercase_platform=$( tr '[:upper:]' '[:lower:]' <<<"$platform" )
+case $lowercase_platform in
+  ios)
+    # generate iOS test report, and export
+    slather coverage --llvm-cov --output-directory "llvm-cov" ${binary_basename:+--binary-basename "$binary_basename"} ${workspace:+--workspace "$ios_workspace"} ${scheme:+--scheme "$ios_scheme"} ${project:+"$ios_project"}
+    mv llvm-cov/report.llcov $BITRISE_DEPLOY_DIR
+    ;;
+  android)
+    # export Android test report
+    mv $android_jacoco_xml $BITRISE_DEPLOY_DIR
+    ;;
+  *)
+    echo "No report exported"
+    ;;
+esac
 
+# trigger sonarqube
 curl -X "POST" "https://$gitpub_sonar_bridge_host/rest/api/1.0/projects/$gitpub_sonar_bridge_owner/repos/$gitpub_sonar_bridge_repo/commits/$gitpub_sonar_bridge_commit_id/comments?path=$gitpub_sonar_bridge_file_path" \
      -H 'Content-Type: application/json; charset=utf-8' \
      -u "$gitpub_username:$gitpub_password" \
